d03221fecbeda1814b96f9071945a184583c2526
ldufour <119424972+lpdbond15@users.noreply.github.com>
2023-09-05 16:50:00 -0400

    readme

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "errno.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "sys/wait.h"
+#include "unistd.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+int	main(int argc, char *argv[])
+{
+int pid = fork();
+	if (pid == -1){
+	} 
+}
