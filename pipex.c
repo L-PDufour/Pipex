0ad1b4ff82a311d6d058ae51bf1c04ac13f6c085
ldufour <119424972+lpdbond15@users.noreply.github.com>
2023-09-10 21:12:49 -0400

    concenate path and free memory

--- pipex.c
+++ pipex.c
@@ -6,26 +6,39 @@
 struct		Pipex
 {
 	char	**path;
-}pipex;
+}			pipex;
 
 int	main(int argc, char *argv[], char **envp)
 {
-	char **path;
-	char *str;
+	char	**path;
+	char	*str;
+	int		i;
+
+	i = 0;
 	path = NULL;
 	while (*envp)
 	{
-		if (ft_strncmp(*envp, "PATH", 4) == 0){
-
+		if (ft_strncmp(*envp, "PATH", 4) == 0)
+		{
 			str = ft_substr(*envp, 5, (ft_strlen(*envp)));
 			pipex.path = ft_split(str, ':');
-			while(*pipex.path)
+			free(str);
+			while (pipex.path[i] != NULL)
 			{
-			printf("%s\n", *pipex.path);
-				pipex.path++;
+				ft_strcat(pipex.path[i], "/");
+				printf("%s\n", pipex.path[i]);
+				i++;
 			}
 		}
 		*envp++;
 	}
+	i = 0;
+	while (pipex.path[i] != NULL)
+	{
+		free(pipex.path[i]);
+		i++;
+	}
+	free(pipex.path);
+	i = 0;
 	return (0);
 }
