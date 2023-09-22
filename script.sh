142260d2aa655c0a8708c568d387616be5046580
L-PDufour <leonpierre.dufour@gmail.com>
2023-09-22 12:47:14 -0400

    Doit enlever les variables dynamiques

--- script.sh
+++ script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-make bonus && ./pipex_bonus infile "cat" "xargs -I {} ping -c 1 {}" "grep PING" outfile1
-< infile cat | xargs -I {} ping -c 1 {} | grep PING > output1
+make bonus && ./pipex_bonus infile "head -n 1" "xargs -I {} ping -c 1 {}" "grep PING" outfile0
+< infile head -n 1 | xargs -I {} ping -c 1 {} | grep PING > output0
+diff outfile0 output0
+if [ $? -eq 0 ]; then 
+	echo "success"
+else 
+	echo "failure"
+fi
+
+./pipex_bonus infile "head -n 1" "xargs -I {} ping -c 1 {}" "grep PING" "wc -w" outfile1
+< infile head -n 1 | xargs -I {} ping -c 1 {} | grep PING | wc -w > output1
 diff outfile1 output1
 if [ $? -eq 0 ]; then 
 	echo "success"
@@ -9,17 +18,8 @@ else
 	echo "failure"
 fi
 
-./pipex_bonus infile "cat" "xargs -I {} ping -c 1 {}" "grep PING" "wc -w" outfile
-< infile cat | xargs -I {} ping -c 1 {} | grep PING | wc -w > output
-diff outfile output
-if [ $? -eq 0 ]; then 
-	echo "success"
-else 
-	echo "failure"
-fi
-
-./pipex_bonus infile "grep a" "grep b" "grep c" "grep d" "grep e" outfile2
-< infile | grep a | grep b | grep c | grep d | grep e > output2
+./pipex_bonus infile "head -n 2" "sort" "uniq" "grep google"  outfile2
+< infile head -n 2 | sort | uniq | grep google > output2
 diff outfile2 output2
 if [ $? -eq 0 ]; then 
 	echo "success"
@@ -27,3 +27,11 @@ else
 	echo "failure"
 fi
 
+# ./pipex_bonus infile "grep 'pattern1'" "sort -r" "head -n 10"  outfile2
+# < infile grep "pattern1" | sort -r | head -n 10 > output3
+# diff outfile3 output3
+# if [ $? -eq 0 ]; then 
+# 	echo "success"
+# else 
+# 	echo "failure"
+# fi
