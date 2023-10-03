58988f1d4312322c454003aa69aee3c1334401b6
Leon-Pierre Dufour <ldufour@c1r4p5.42quebec.com>
2023-10-03 15:22:06 -0400

    norminette

--- bonus/bonus_utils.c
+++ bonus/bonus_utils.c
@@ -26,7 +26,7 @@ void	file_creation_bonus(char **argv, t_pipex *pipex, int argc)
 {
 	pipex->infile = open(argv[1], O_RDONLY);
 	pipex->outfile = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	// exit_pipex(pipex->infile, "Error with infile", pipex);
+	exit_pipex(pipex->infile, "Error with infile", pipex);
 	exit_pipex(pipex->outfile, "Error with outfile", pipex);
 }
 
@@ -71,7 +71,7 @@ void	wait_process(t_pipex *pipex, int process_nb)
 	i = -1;
 	while (++i < process_nb)
 	{
-		exit_pipex(waitpid(pipex->pids_bonus[i], &status, 0), "waitpid failed", pipex);
+		exit_pipex(waitpid(pipex->pids_bonus[i], &status, 0), "waitpid", pipex);
 	}
 	free(pipex->pids_bonus);
 }
